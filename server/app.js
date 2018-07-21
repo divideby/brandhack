7dc4abeb8f07e2261499c1e799f7ed0a87786e89
Dmitriy <divideby@yandex.ru>
2018-07-21 12:24:56 +0800

    remove useless

--- server/app.js
+++ server/app.js
@@ -6,22 +6,32 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.disable("etag");
 
+const allowCrossDomain = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+app.use(allowCrossDomain);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+/*
+ * API
+ */
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
